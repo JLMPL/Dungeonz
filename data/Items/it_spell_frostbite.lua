292ae06aa99dbc0ef4b9ad4452f11fea18adfc94
Dominik <assshitfuck@gmail.com>
2017-04-28 15:50:15 +0200

    I think its ready

--- data/Items/it_spell_frostbite.lua
+++ data/Items/it_spell_frostbite.lua
@@ -11,7 +11,7 @@ Item = {
 	effect = function(user)
 		if user:knowsSpell(Spell_Frostbite) == false then
 			user:learnSpell(Spell_Frostbite, true)
-			gui_read("Frosty saves the day!");
+			gui_read("Cold like this can stop a \nheartbeat in a matter of seconds.\nBlur perception and stun.");
 		end
 	end
 }
